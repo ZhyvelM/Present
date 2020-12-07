6abe7f9247e4e686f4be449b938c4e1ad8769db2
Maksim <zhyvelm@gmail.com>
2020-12-07 11:39:47 +0300

    structure creation

---
+++ Present/Candy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Present
+{
+    public abstract class Candy
+    {
+        public string name;
+        public string manufacturer;
+        public double weight;
+        public double sugar;
+        public int count;
+
+        public Candy(string name, string manufacturer, double weight, double sugar, int count)
+        {
+            this.name = name;
+            this.manufacturer = manufacturer;
+            this.weight = weight;
+            this.sugar = sugar;
+            this.count = count;
+        }
+    }
+}
