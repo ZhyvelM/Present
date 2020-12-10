a8df1dad7111ef12d6153486191ea1e2b1810ce1
Maksim <zhyvelm@gmail.com>
2020-12-10 14:20:38 +0300

    Sorters replace

---
+++ Present/CandySorters/CandyByManufacturerSorter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Present.Sorters
+{
+    class CandyByManufacturerSorter : IComparer<Candy>
+    {
+        public int Compare([AllowNull] Candy x, [AllowNull] Candy y)
+        {
+            return x.Manufacturer.CompareTo(y.Manufacturer);
+        }
+    }
+}
---
+++ Present/CandySorters/CandyBySugarSorter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Present.Sorters
+{
+    class CandyBySugarSorter : IComparer<Candy>
+    {
+        public int Compare([AllowNull] Candy x, [AllowNull] Candy y)
+        {
+            if (x.Sugar < y.Sugar) return 1;
+            else if (x.Sugar > y.Sugar) return -1;
+            else return 0;
+        }
+    }
+}
